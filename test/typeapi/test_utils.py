6442e40498bb55f49c0e1167e92980096446ec60
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-03-14 11:12:39 +0100

    Final does not exist in 3.7

--- test/typeapi/test_utils.py
+++ test/typeapi/test_utils.py
@@ -19,6 +19,7 @@ def test_is_generic_alias():
 
 
 def test_is_special_form():
+  assert not is_special_form(t.Any)
   assert is_special_form(t.ClassVar)
   assert is_special_form(t.Union)
   assert not is_special_form(t.List)
@@ -41,4 +42,4 @@ def test_get_special_generic_aliases():
 
 
 def test_get_special_forms():
-  assert set(get_special_forms().keys()).issuperset(['Union', 'Final', 'ClassVar'])
+  assert set(get_special_forms().keys()).issuperset(['Union', 'NoReturn', 'ClassVar'])
