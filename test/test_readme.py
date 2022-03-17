191247edaa0a1cd0a31f8a801e3edcba4cca0f3b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-03-17 03:50:47 +0100

    fix `test_readme.py` for older python versions

--- test/test_readme.py
+++ test/test_readme.py
@@ -1,9 +1,10 @@
 
 import collections.abc
 from typeapi import *
+import typing_extensions as te
 
 
 def test_readme_example():
   import typing, typeapi
-  value = typeapi.of(typing.Mapping[typing.Annotated[str, "key"], typing.Literal[True, None, 'false']])
+  value = typeapi.of(typing.Mapping[te.Annotated[str, "key"], typing.Literal[True, None, 'false']])
   assert value == Type(collections.abc.Mapping, nparams=2, args=(Annotated(Type(str, nparams=0), ('key',)), Literal(values=(True, None, 'false'))))
