0225e55f15859137f7b7e7b9c799aafda2f90768
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-03-22 22:26:59 +0100

    release 0.1.2

--- src/typeapi/__init__.py
+++ src/typeapi/__init__.py
@@ -18,7 +18,7 @@ The #typeapi.of() function introspects the type hint passed as an argument and c
 using the dataclasses defined in #typeapi.model.
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .model import Hint, Type, Union, Annotated, ForwardRef, Any, ClassVar, Final, NoReturn, TypeGuard, Literal, \
     NewType, Unknown, eval_types, infuse_type_parameters
