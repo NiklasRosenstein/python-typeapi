6650bf8807e3d25b4a474a62bc7d8fd81b874861
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-07-03 10:47:24 +0200

    update

--- src/typeapi/typehint.py
+++ src/typeapi/typehint.py
@@ -93,7 +93,9 @@ class _TypeHintMeta(abc.ABCMeta):
         elif origin == tuple:
             return TupleTypeHint(hint, source)
 
-        elif origin is None and type(hint).__name__ == "_TypeAliasBase":  # Python <3.10
+        elif origin is None and type(hint).__name__ == "_TypeAliasBase":  # Python 3.6
+            return TypeAliasTypeHint(hint, source)
+        elif origin is None and getattr(hint, "_name") == "TypeAlias":  # Python <3.10
             return TypeAliasTypeHint(hint, source)
         elif origin is None and getattr(hint, "__name__", None) == "TypeAlias":  # Python >=3.10
             return TypeAliasTypeHint(hint, source)
