adc5bd6c9d531515d36b896332d3272e97ef651b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-18 13:17:02 +0100

    Migrate to Mkdocs and Mkdocstrings

--- src/typeapi/__init__.py
+++ src/typeapi/__init__.py
@@ -10,7 +10,7 @@ from .typehint import (
     TypeVarTypeHint,
     UnionTypeHint,
 )
-from .utils import TypedDictProtocol, get_annotations, is_typed_dict, type_repr
+from .utils import get_annotations, is_typed_dict, type_repr
 
 __all__ = [
     # .typehint
@@ -26,5 +26,4 @@ __all__ = [
     "get_annotations",
     "is_typed_dict",
     "type_repr",
-    "TypedDictProtocol",
 ]
